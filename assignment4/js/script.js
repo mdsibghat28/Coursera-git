2c402c9742bf175c017e3a06cdb3699076b64737
Sibghatullah <mdsibghat28@gmail.com>
2020-08-11 19:38:45 +0530

    scripts

---
+++ assignment4/js/script.js
@@ -0,0 +1,11 @@
+(function() {
+    var names = ["kane", "smith", "root", "buttler", "stokes", "woakes", "morgan"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
